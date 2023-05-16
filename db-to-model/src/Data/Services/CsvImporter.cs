a3078165088f987b32cb6484bee89a10100d65a2
Maurizio Napoleoni <napy84@gmail.com>
2023-05-16 15:04:33 +1000

    diary-example: csv reader in Data project

---
+++ db-to-model/src/Data/Services/CsvImporter.cs
@@ -0,0 +1,49 @@
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using FoodDiary.Core.Dto;
+
+namespace FoodDiary.Data.Services;
+
+public class CsvImporter
+{
+    public CsvImporter()
+    {
+    }
+
+    public List<FoodWithAmountDto> ReadCsv(string filePath)
+    {
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HeaderValidated = null,
+            IgnoreBlankLines = true
+        };
+
+        // Read the CSV file using CsvHelper
+        using var reader = new StreamReader(filePath);
+        using var csv = new CsvReader(reader, config);
+        var actualData = csv.GetRecords<FoodCsvFormat>().ToList();
+
+        List<FoodWithAmountDto> foodWithAmountDtos = actualData.Select(data => new FoodWithAmountDto(
+              Name: data.FoodName,
+              FoodAmount: new FoodAmountDto(
+                    Amount: data.Amount,
+                    Protein: data.Protein,
+                    Fat: data.Fat,
+                    Carbohydrates: data.Carbs,
+                    Fiber: data.Fiber,
+                    Alcohol: data.Alcohol ?? 0,
+                    Sugar: data.Sugar ?? 0,
+                    SaturatedFats: data.SaturatedFats ?? 0,
+                    Sodium: data.Sodium ?? 0,
+                    Cholesterol: data.Cholesterol ?? 0,
+                    Potassium: data.Potassium ?? 0,
+                    Iron: data.Iron ?? 0,
+                    Calcium: data.Calcium ?? 0,
+                    Source: data.Source,
+                    AmountName: $"{data.Amount} - ${data.FoodName}"
+              )
+            )).ToList();
+        return foodWithAmountDtos;
+    }
+}
--- db-to-model/test/Unit/Samples/FileRead/CsvFileReadTest.cs
+++ db-to-model/test/Unit/Samples/FileRead/CsvFileReadTest.cs
@@ -1,9 +1,7 @@
-using System.Globalization;
 using System.Reflection;
-using CsvHelper;
-using CsvHelper.Configuration;
 using FluentAssertions;
 using FoodDiary.Core.Dto;
+using FoodDiary.Data.Services;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -28,53 +26,24 @@ public class FileTest
         var testFileShould = File.Exists(filePath);
         testFileShould.Should().BeTrue();
 
-        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        var csvImporter = new CsvImporter();
+        List<FoodWithAmountDto> foods = csvImporter.ReadCsv(filePath);
+        foreach (var data in foods)
         {
-            HeaderValidated = null,
-            IgnoreBlankLines = true
-        };
-
-        // Read the CSV file using CsvHelper
-        using var reader = new StreamReader(filePath);
-        using var csv = new CsvReader(reader, config);
-        var actualData = csv.GetRecords<FoodCsvFormat>().ToList();
-        foreach (var data in actualData)
-        {
-            FoodWithAmountDto foodWithAmountDto = new FoodWithAmountDto(
-              Name: data.FoodName,
-              FoodAmount: new FoodAmountDto(
-                    Amount: data.Amount,
-                    Protein: data.Protein,
-                    Fat: data.Fat,
-                    Carbohydrates: data.Carbs,
-                    Fiber: data.Fiber,
-                    Alcohol: data.Alcohol ?? 0,
-                    Sugar: data.Sugar ?? 0,
-                    SaturatedFats: data.SaturatedFats ?? 0,
-                    Sodium: data.Sodium ?? 0,
-                    Cholesterol: data.Cholesterol ?? 0,
-                    Potassium: data.Potassium ?? 0,
-                    Iron: data.Iron ?? 0,
-                    Calcium: data.Calcium ?? 0,
-                    Source: data.Source,
-                    AmountName: $"{data.Amount} - ${data.FoodName}"
-              )
-            );
-
-            foodWithAmountDto.Name.Should().NotBeEmpty();
-            foodWithAmountDto.FoodAmount.Protein.Should().BeGreaterThanOrEqualTo(0);
-            foodWithAmountDto.FoodAmount.Fat.Should().BeGreaterThanOrEqualTo(0);
-            foodWithAmountDto.FoodAmount.Carbohydrates.Should().BeGreaterThanOrEqualTo(0);
-            foodWithAmountDto.FoodAmount.Fiber.Should().BeGreaterThanOrEqualTo(0);
-            foodWithAmountDto.FoodAmount.Alcohol.Should().BeGreaterThanOrEqualTo(0);
-            foodWithAmountDto.FoodAmount.Sugar.Should().BeGreaterThanOrEqualTo(0);
-            foodWithAmountDto.FoodAmount.SaturatedFats.Should().BeGreaterThanOrEqualTo(0);
-            foodWithAmountDto.FoodAmount.Sodium.Should().BeGreaterThanOrEqualTo(0);
-            foodWithAmountDto.FoodAmount.Cholesterol.Should().BeGreaterThanOrEqualTo(0);
-            foodWithAmountDto.FoodAmount.Potassium.Should().BeGreaterThanOrEqualTo(0);
-            foodWithAmountDto.FoodAmount.Iron.Should().BeGreaterThanOrEqualTo(0);
-            foodWithAmountDto.FoodAmount.Calcium.Should().BeGreaterThanOrEqualTo(0);
-            foodWithAmountDto.FoodAmount.Source.Should().NotBe("CSV NOT PRESENT");
+            data.Name.Should().NotBeEmpty();
+            data.FoodAmount.Protein.Should().BeGreaterThanOrEqualTo(0);
+            data.FoodAmount.Fat.Should().BeGreaterThanOrEqualTo(0);
+            data.FoodAmount.Carbohydrates.Should().BeGreaterThanOrEqualTo(0);
+            data.FoodAmount.Fiber.Should().BeGreaterThanOrEqualTo(0);
+            data.FoodAmount.Alcohol.Should().BeGreaterThanOrEqualTo(0);
+            data.FoodAmount.Sugar.Should().BeGreaterThanOrEqualTo(0);
+            data.FoodAmount.SaturatedFats.Should().BeGreaterThanOrEqualTo(0);
+            data.FoodAmount.Sodium.Should().BeGreaterThanOrEqualTo(0);
+            data.FoodAmount.Cholesterol.Should().BeGreaterThanOrEqualTo(0);
+            data.FoodAmount.Potassium.Should().BeGreaterThanOrEqualTo(0);
+            data.FoodAmount.Iron.Should().BeGreaterThanOrEqualTo(0);
+            data.FoodAmount.Calcium.Should().BeGreaterThanOrEqualTo(0);
+            data.FoodAmount.Source.Should().NotBe("CSV NOT PRESENT");
         }
     }
 }
