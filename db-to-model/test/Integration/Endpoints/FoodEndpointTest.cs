ae64ffbd3def7cd1c1ad79740dadd9c3384d7fa3
Maurizio Napoleoni <napy84@gmail.com>
2023-05-29 10:04:44 +1000

    diary-example: Testing database queries

--- db-to-model/test/Integration/Endpoints/FoodEndpointTest.cs
+++ db-to-model/test/Integration/Endpoints/FoodEndpointTest.cs
@@ -1,13 +1,17 @@
+using System.Linq.Expressions;
 using System.Net;
 using System.Text.Json;
+using CsvHelper.Configuration;
 using FluentAssertions;
 using FoodDiary.Api.Models;
 using FoodDiary.Core.Entities;
 using FoodDiary.Data.Contexts;
 using Integration.Helpers;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit;
+using Xunit.Abstractions;
 using static Integration.ConfigureWebApplicationFactory;
 
 namespace Integration.Endpoints;
@@ -15,6 +19,8 @@ namespace Integration.Endpoints;
 public class FoodEndpointTest : IClassFixture<CustomWebApplicationFactory<Program>>
 {
     private readonly HttpClient _httpClient;
+
+    private readonly ITestOutputHelper testOutputHelper;
     private readonly CustomWebApplicationFactory<Program> factory;
     private readonly FoodDiaryDbContext dbContext;
     private readonly JsonSerializerOptions jsonSerializerOptions = new()
@@ -22,10 +28,11 @@ public class FoodEndpointTest : IClassFixture<CustomWebApplicationFactory<Progra
         PropertyNameCaseInsensitive = true
     };
 
-    public FoodEndpointTest(CustomWebApplicationFactory<Program> _factory)
+    public FoodEndpointTest(CustomWebApplicationFactory<Program> _factory, ITestOutputHelper _testOutputHelper)
     {
 
         factory = _factory;
+        testOutputHelper = _testOutputHelper;
         var scope = _factory.Services.CreateScope();
         var scopedServices = scope.ServiceProvider;
         dbContext = scopedServices.GetRequiredService<FoodDiaryDbContext>();
@@ -59,4 +66,26 @@ public class FoodEndpointTest : IClassFixture<CustomWebApplicationFactory<Progra
         }
     }
 
+    [Theory(DisplayName = "Get Food By Multiple Name Parts")]
+    [InlineData("woolworths tasty", "Woolworths Light Tasty Shredded Cheese (100g)")]
+    [InlineData("Banana cavendish", "Banana cavendish peeled raw")]
+    [InlineData("Cavendish Banana", "Banana cavendish peeled raw")]
+    [InlineData("97% Fat", "Bacon Short Cut 97% Fat Free (Aldi)")]
+    [InlineData("Fat 97%", "Bacon Short Cut 97% Fat Free (Aldi)")]
+    [InlineData("Free 97% Fat Short", "Bacon Short Cut 97% Fat Free (Aldi)")]
+    [InlineData("Cut 97", "Bacon Short Cut 97% Fat Free (Aldi)")]
+    [InlineData("Margarine olive oil", "Margarine spread olive oil blend 65% fat reduced salt sodium 360mg/100 g")]
+    public async Task GetFood(string search, string expected)
+    {
+        IQueryable<Food> query = dbContext.Foods;
+        var searchTerms = search.Split(' ').ToList();
+        foreach (var term in searchTerms)
+        {
+            query = query.Where(f => EF.Functions.ILike(f.Name, '%' + term + '%'));
+        }
+        var results = await query.ToListAsync();
+
+        results.Count.Should().Be(1);
+        results[0].Name.Should().Be(expected);
+    }
 }
