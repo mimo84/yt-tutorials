3a5b14e80f655d64083846b0c34d94d23d720f95
Maurizio Napoleoni <napy84@gmail.com>
2024-07-06 17:17:00 +1000

    Debug in progress

--- iac-linode/terraform/main.tf
+++ iac-linode/terraform/main.tf
@@ -15,11 +15,18 @@ data "tls_public_key" "private_key_openssh-linode" {
 resource "linode_instance" "web_server" {
   image           = "linode/ubuntu24.04"
   label           = "linode-terraform"
-  region          = "eu-west"
+  region          = "ap-southeast"
   type            = "g6-nanode-1"
   tags            = ["hello-terraform"]
   root_pass       = var.linode_root_password
   authorized_keys = [chomp(data.tls_public_key.private_key_openssh-linode.public_key_openssh)]
+
+  connection {
+    type        = "ssh"
+    user        = "root"
+    host        = self.public_ip
+    private_key = file(var.linode_root_ssh_privatekey)
+  }
 
   provisioner "local-exec" {
     command = "export ANSIBLE_HOST_KEY_CHECKING=False && ansible-playbook -u root -i '${self.ip_address},' --private-key='${var.linode_root_ssh_privatekey}' ../ansible/setup.yml"
